c9b616411272947822cc0a9b18a3dc9a4ce74908
Mohammad Usman <46886908+mohammadusman666@users.noreply.github.com>
2020-08-14 10:44:01 +0500

    Redefined Components using Arrow functions

--- src/App.js
+++ src/App.js
@@ -23,32 +23,30 @@ function getTitle(title) {
   return title;
 }
 
-function App() {
-  return (
-    <div>
-      <h1>Hello {getTitle('React')}</h1>
+const App = () => (
+  <div>
+    <h1>Hello {getTitle('React')}</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" />
 
-      <hr />
+    <hr />
 
-      <List />
+    <List />
+  </div>
+)
+
+const List = () => (
+  list.map(item => (
+    <div key={ item.objectID }>
+      <span>
+        <a href={ item.url }>{ item.title }</a>
+      </span>
+      <span>{ item.num_comments }</span>
+      <span>{ item.author }</span>
+      <span>{ item.points }</span>
     </div>
-  );
-}
-
-function List() {
-  return list.map(item => {
-    return <div key={ item.objectID }>
-            <span>
-              <a href={ item.url }>{ item.title }</a>
-            </span>
-            <span>{ item.num_comments }</span>
-            <span>{ item.author }</span>
-            <span>{ item.points }</span>
-           </div>;
-  })
-}
+  ))
+)
 
 export default App;
