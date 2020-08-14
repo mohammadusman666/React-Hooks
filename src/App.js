a56771ef213392e483e317c34defe842fd042298
Mohammad Usman <46886908+mohammadusman666@users.noreply.github.com>
2020-08-14 10:20:39 +0500

    Lists in React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 import React from 'react';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 function getTitle(title) {
   return title;
 }
@@ -11,6 +30,21 @@ function App() {
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+
+      <hr />
+
+      {
+        list.map(item => {
+          return <div key={ item.objectID }>
+                  <span>
+                    <a href={ item.url }>{ item.title }</a>
+                  </span>
+                  <span>{ item.num_comments }</span>
+                  <span>{ item.author }</span>
+                  <span>{ item.points }</span>
+                 </div>;
+        })
+      }
     </div>
   );
 }
