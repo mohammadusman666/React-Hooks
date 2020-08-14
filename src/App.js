d9c54e13665d3288e949dd1bbcb72b72bc6dac14
Mohammad Usman <46886908+mohammadusman666@users.noreply.github.com>
2020-08-14 10:29:06 +0500

    List Component

--- src/App.js
+++ src/App.js
@@ -33,20 +33,22 @@ function App() {
 
       <hr />
 
-      {
-        list.map(item => {
-          return <div key={ item.objectID }>
-                  <span>
-                    <a href={ item.url }>{ item.title }</a>
-                  </span>
-                  <span>{ item.num_comments }</span>
-                  <span>{ item.author }</span>
-                  <span>{ item.points }</span>
-                 </div>;
-        })
-      }
+      <List />
     </div>
   );
+}
+
+function List() {
+  return list.map(item => {
+    return <div key={ item.objectID }>
+            <span>
+              <a href={ item.url }>{ item.title }</a>
+            </span>
+            <span>{ item.num_comments }</span>
+            <span>{ item.author }</span>
+            <span>{ item.points }</span>
+           </div>;
+  })
 }
 
 export default App;
